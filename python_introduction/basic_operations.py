90c5aac3353bcc95cc824fbe442fd632abbdf70f
Haile Kenasa Tafa <142696185+hailekenasatafa@users.noreply.github.com>
2024-08-15 23:45:30 +0300

    Update basic_operations.py
    I add print statement

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -5,3 +5,9 @@ number2 = 5
 sum = number1 + number2 
 difference  = number1 - number2
 product = number1 * number2
+
+
+print("sum of", number1, "and", number2, "is", sum, ".")
+print("difference of", number1, "and", number2, "is", difference, ".")
+print("product of", number1, "and", number2, "is", product, ".")
+
